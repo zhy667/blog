be219b8aca303e2d5368917760e3c7db3f09fd21
zhy667 <hongyu.zhang@empatiohq.com>
2024-06-03 11:25:45 +0800

    练习

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import Account from './components/Account.vue'
-import Auth from './components/Auth.vue'
-import { supabase } from './supabase'
-
-const session = ref()
-
-onMounted(() => {
-  supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
-
-  supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="container" style="padding: 50px 0 100px 0">
-    <Account v-if="session" :session="session" />
-    <Auth v-else />
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- supabase-vue-3/src/components/HelloWorld.vue
+++ supabase-vue-3/src/components/HelloWorld.vue
@@ -5,7 +5,7 @@ defineProps({
   msg: String,
 })
 
-const count = ref(10)
+const count = ref(0)
 </script>
 
 <template>
