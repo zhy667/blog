d9f533efe3bce59045732ff34f10c9d7ffe1185b
zhy667 <hongyu.zhang@empatiohq.com>
2024-06-07 22:31:42 +0800

    日志67

---
+++ my-vue-project/src/components/第一次写.vue
@@ -0,0 +1,17 @@
+<script type="importmap">
+ 
+</script>
+
+<div id="app">{{ message }}</div>
+
+<script type="module">
+  import { createApp } from 'vue'
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+</script>
