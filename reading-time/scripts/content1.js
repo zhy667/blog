a49509a180ff15aa4fcb664127758003ce9f39e5
zhy667 <hongyu.zhang@empatiohq.com>
2024-06-15 18:24:26 +0800

    success

---
+++ reading-time/scripts/content1.js
@@ -0,0 +1,17 @@
+  {
+  
+  
+  {elementList = document.querySelector(".ng-tns-c1798928316-92");
+
+
+ 
+    document.querySelector(".ng-tns-c1798928316-92")
+    const ele = document.createElement("button")
+    const target =document.querySelector(".ng-tns-c1798928316-92")
+    ele.innerText = "click me"
+    document.addEventListener("click", function(){
+      document.getElementById("demo").addEventListener("click", myFunction);
+  });
+    target.insertAdjacentElement("afterend",ele)
+  }
+}
