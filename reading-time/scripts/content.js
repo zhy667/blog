155ec07ffd8af6e990ec2148e81f75c491c31709
zhy667 <hongyu.zhang@empatiohq.com>
2024-06-15 12:45:28 +0800

    697

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -1,32 +1,13 @@
-const article = document.querySelector("article");
+{const article = document.querySelector(".ng-tns-c1798928316-91");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
 
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
-  alert(123)
  
-  document.querySelector(".ng-tns-c1798928316-91").textContent
+  document.querySelector(".ng-tns-c1798928316-91")
   const ele = document.createElement("button")
   const target =document.querySelector(".ng-tns-c1798928316-91")
   ele.innerText = "click me"
   document.addEventListener("click", function(){
-    document.getElementById(".ng-tns-c1798928316-91").innerHTML = "Hello World!";
+    document.getElementById("demo").addEventListener("click", myFunction);
 });
   target.insertAdjacentElement("afterend",ele)
 }
