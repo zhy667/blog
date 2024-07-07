55ea2a3fef4000f00ed70449b60199cab57adf4e
zhy667 <hongyu.zhang@empatiohq.com>
2024-07-07 12:55:20 +0800

    vue练习

---
+++ 2024707.vue
@@ -0,0 +1,47 @@
+<template>
+
+
+<img src="https://images.pexels.com/photos/15804651/pexels-photo-15804651.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width="1550" height="1300" alt="">
+
+
+
+
+
+
+</template>
+
+
+
+<script>
+
+
+
+
+
+
+
+
+
+
+
+</script>
+<style>
+
+.img{
+ display: flex;   
+align-items: center;
+justify-self: center;
+height: 700;
+
+
+
+}
+
+
+
+
+
+
+
+
+</style>
